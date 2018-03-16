a735de541569653a8e12708d7e0bcd0fa1f0343a
tmlst <johnyjohn018@gmail.com>
2018-03-16 17:14:29 +0300

    copy json from assets to internal storage at first app launch

---
+++ app/src/main/java/com/tmlst/testtask/timetableapp/FileHelper.java
@@ -0,0 +1,75 @@
+package com.tmlst.testtask.timetableapp;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by User on 16.03.2018.
+ */
+
+public class FileHelper {
+
+    private Context context;
+    private String fileName = null;
+    private static final String JSON_FILE_NAME = "allStations.json";
+
+    public FileHelper(Context context) {
+
+        this.context = context;
+        fileName = context.getResources().getString(R.string.json_file_name);
+
+//        File dir = context.getFilesDir();
+//        File file = new File(dir, fileName);
+//        boolean deleted = file.delete();
+    }
+
+    //    Читаем json из памяти и возвращаем в виде строки
+    public String getJsonString() {
+
+        try {
+            context.openFileInput(fileName);
+        } catch (FileNotFoundException e) {
+            copyJsonFile();
+//            Toast.makeText(context, "copy json from assets",
+//                    Toast.LENGTH_SHORT).show();
+        }
+
+        FileInputStream inputStream;
+        byte buff[] = new byte[1];
+        try {
+            inputStream = context.openFileInput(fileName);
+            buff = new byte[inputStream.available()];
+            int len = inputStream.read(buff);
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new String(buff);
+    }
+
+    //    Копируем файл json из каталога assets в internal storage
+    private void copyJsonFile() {
+        InputStream inputStream;
+        OutputStream outputStream;
+        try {
+            inputStream = context.getAssets().open(JSON_FILE_NAME);
+            outputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- app/src/main/java/com/tmlst/testtask/timetableapp/MainActivity.java
+++ app/src/main/java/com/tmlst/testtask/timetableapp/MainActivity.java
@@ -1,77 +1,96 @@
 package com.tmlst.testtask.timetableapp;
 
 import android.app.Activity;
-import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends Activity {
-
-    private String fileName = null;
-    private static final String JSON_FILE_NAME = "allStations.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, getJsonString().substring(3000000, 3000100),
-                    Toast.LENGTH_SHORT).show();
+        new ParseJsonTask().execute();
+
+//        FileHelper fileHelper = new FileHelper(this);
+//        String jsonAsString = fileHelper.getJsonString();
+//        Toast.makeText(this, jsonAsString.substring(3000000, 3000100),
+//                    Toast.LENGTH_SHORT).show();
 
     }
 
-//    Читаем json из памяти и возвращаем в виде строки
-    private String getJsonString() {
+    private class ParseJsonTask extends AsyncTask<Void, Void, String> {
 
-        fileName = getResources().getString(R.string.json_file_name);
-
-        FileInputStream inputStream;
-        byte buff[] = new byte[1];
-        try {
-            inputStream = openFileInput(fileName);
-            buff = new byte[inputStream.available()];
-            int len = inputStream.read(buff);
-            inputStream.close();
-
-//            Toast.makeText(this, String.valueOf(len),
-//                    Toast.LENGTH_SHORT).show();
-//            Toast.makeText(this, new String(buff, 0, 20),
-//                    Toast.LENGTH_SHORT).show();
-
-        } catch (FileNotFoundException fnfe) {
-
-            //Toast.makeText(this, "file's not exist, downloading it",
-              //      Toast.LENGTH_SHORT).show();
-
-            downloadJsonFile();
-        } catch (IOException e) {
-            e.printStackTrace();
+        @Override
+        protected String doInBackground(Void... voids) {
+            FileHelper fileHelper = new FileHelper(MainActivity.this);
+            return fileHelper.getJsonString();
         }
 
-        return new String(buff);
-    }
+        @Override
+        protected void onPostExecute(String jsonString) {
+            super.onPostExecute(jsonString);
 
-//    Копируем файл json из каталога assets в internal storage
-    private void downloadJsonFile() {
-        InputStream inputStream;
-        OutputStream outputStream;
-        try {
-            inputStream = getAssets().open(JSON_FILE_NAME);
-            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            JSONObject dataJsonObj;
+            String firstCityName;
 
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, len);
+            try {
+
+                dataJsonObj = new JSONObject(jsonString);
+                JSONArray citiesFrom = dataJsonObj.getJSONArray("citiesFrom");
+
+                // выведем название первого города отправления
+                JSONObject firstCityFrom = citiesFrom.getJSONObject(0);
+                firstCityName = firstCityFrom.getString("cityTitle");
+
+                Toast.makeText(MainActivity.this, firstCityName,
+                        Toast.LENGTH_SHORT).show();
+
+                // выведем название 11-го города отправления
+                firstCityFrom = citiesFrom.getJSONObject(10);
+                firstCityName = firstCityFrom.getString("cityTitle");
+
+                Toast.makeText(MainActivity.this, firstCityName,
+                        Toast.LENGTH_SHORT).show();
+
+                // перечислим его станции
+                JSONArray eleventhCityStations = firstCityFrom.
+                        getJSONArray("stations");
+
+                for (int i = 0; i < eleventhCityStations.length(); i++) {
+                    JSONObject station = eleventhCityStations.getJSONObject(i);
+
+                    JSONObject point = station.getJSONObject("point");
+                    String longitude = point.getString("longitude");
+                    String latitude = point.getString("latitude");
+
+                    String countryTitle = station.getString("countryTitle");
+                    String districtTitle = station.getString("districtTitle");
+                    String cityId = station.getString("cityId");
+                    String cityTitle = station.getString("cityTitle");
+                    String regionTitle = station.getString("regionTitle");
+                    String stationId = station.getString("stationId");
+                    String stationTitle = station.getString("stationTitle");
+
+                    String stationInfo = countryTitle + "\n" + longitude + "\n" +
+                            latitude + "\n" + districtTitle + "\n" + cityId + "\n" +
+                            cityTitle + "\n" + regionTitle + "\n" + stationId + "\n" +
+                            stationTitle;
+
+                    Toast.makeText(MainActivity.this, stationInfo,
+                            Toast.LENGTH_LONG).show();
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
